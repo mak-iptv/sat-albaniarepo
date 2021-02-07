53f9fd3fe43c7ad1e0a346f002cca54c66bd7dbf
mak-iptv <49493392+mak-iptv@users.noreply.github.com>
2021-02-07 20:54:09 +0100

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 clear
 echo " "
-echo -e "${txtblue} ┌────────────────────────────────────────────┐ "
-echo -e "${txtblue} │[R]        Welcome to  Xtream Panel          │ "
-echo -e "${txtblue} └────────────────────────────────────────────┘ "
+echo -e "${txtyellow} ┌────────────────────────────────────────────┐ "
+echo -e "${txtyellow} │[R]        Welcome to Brutus Panel          │ "
+echo -e "${txtyellow} └────────────────────────────────────────────┘ "
 PS3='Please enter your choice: '
 options=("https installation" "http installation" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
         "https installation")
-            wget -q -O installalbi.sh https://raw.githubusercontent.com/mak-iptv/sat-albaniarepo/main/installalbi.sh && chmod +x installalbi.sh && ./installalbi.sh 
+            wget -q -O installalbi.sh https://raw.githubusercontent.com/mak-iptv/sat-albaniarepo/main/installalbi.sh && chmod +x installalbi.sh && ./installalbi.sh
             break
             ;;
         "http installation")
-            wget -q -O installx.sh  https://raw.githubusercontent.com/mak-iptv/sat-albaniarepo/main/installx.sh && chmod +x installx.sh  && ./installx.sh 
+            wget -q -O installx.sh https://raw.githubusercontent.com/mak-iptv/sat-albaniarepo/main/installx.sh && chmod +x installx.sh && ./installx.sh
             break
             ;;
         "Quit")
