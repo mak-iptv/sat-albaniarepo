37490688eea793d9def65958a6179c8e82bb2930
mak-iptv <49493392+mak-iptv@users.noreply.github.com>
2021-02-07 20:14:37 +0100

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -2,7 +2,7 @@
 clear
 echo " "
 echo -e "${txtblue} ┌────────────────────────────────────────────┐ "
-echo -e "${txtblue} │[R]        Welcome to Brutus Panel          │ "
+echo -e "${txtblue} │[R]        Welcome to  Xtream Panel          │ "
 echo -e "${txtblue} └────────────────────────────────────────────┘ "
 PS3='Please enter your choice: '
 options=("https installation" "http installation" "Quit")
@@ -10,7 +10,7 @@ select opt in "${options[@]}"
 do
     case $opt in
         "http installation")
-            wget -q -O https.sh https://raw.githubusercontent.com/mak-iptv/sat-albaniarepo/main/installalbi.sh && chmod +x menu.sh && ./menu.sh 
+            wget -q -O installalbi.sh https://raw.githubusercontent.com/mak-iptv/sat-albaniarepo/main/installalbi.sh && chmod +x installalbi.sh && ./installalbi.sh 
             break
             ;;
         "http installation")
